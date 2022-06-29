c7d785a491a4b6bfcaafb9953b1e8d12106714d0
rafabrazil <rafael.h@outlook.com>
2022-06-29 14:09:15 -0300

    página event responsiva

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -39,12 +39,12 @@ export function Video(props: VideoProps) {
                 </div>
             </div>
             <div className="p-8 max-w-[1100px] mx-auto">
-                <div className="flex items-start gap-16">
+                <div className="flex flex-col md:flex-row md:items-start :gap-16">
                     <div className="flex-1">
-                        <h1 className="text-2xl font-bold">
+                        <h1 className="text-lg md:text-2xl font-bold">
                             {data.lesson.title}
                         </h1>
-                        <p className="mt-4 text-gray-200 leading-relaxed">
+                        <p className="mt-4 text-gray-200 text-sm md:text-base leading-relaxed">
                             {data.lesson.description}
                         </p>
                         {data.lesson.teacher && (
@@ -55,7 +55,7 @@ export function Video(props: VideoProps) {
                                     alt=""
                                 />
                                 <div className="leading-relaxed">
-                                    <strong className="font-bold text-2xl block">
+                                    <strong className="font-bold text-lg md:text-2xl block">
                                         {data.lesson.teacher.name}
                                     </strong>
                                     <span className="text-gray-200 text-sm block">
@@ -65,7 +65,7 @@ export function Video(props: VideoProps) {
                             </div>
                         )}
                     </div>
-                    <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-4 mt-6 md:mt-0">
                         <a href="" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
                             <DiscordLogo size={24} />
                             Comunidade do Discord
@@ -77,32 +77,32 @@ export function Video(props: VideoProps) {
                     </div>
 
                 </div>
-                <div className="gap-8 mt-20 grid grid-cols-2">
-                    <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                <div className="gap-8 mt-20 grid grid-rows-2 md:grid-rows-none md:grid-cols-2">
+                    <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-2 md:gap-6 hover:bg-gray-600 transition-colors">
                         <div className="bg-green-700 h-full p-6 flex items-center">
                             <FileArrowDown size={40} />
                         </div>
                         <div className="py-6 leading-relaxed">
-                            <strong className="text-2xl">Material complementar</strong>
+                            <strong className="text-lg md:text-2xl">Material complementar</strong>
                             <p className="text-sm text-gray-200 mt-2">
                                 Acesse o material complementar para acelerar o seu desenvolvimento
                             </p>
                         </div>
-                        <div className="h-full p-6 flex items-center">
+                        <div className="h-full p-6 flex items-center text-blue-500">
                             <CaretRight size={24} />
                         </div>
                     </a>
-                    <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                    <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-2 md:gap-6 hover:bg-gray-600 transition-colors">
                         <div className="bg-green-700 h-full p-6 flex items-center">
                             <FileArrowDown size={40} />
                         </div>
                         <div className="py-6 leading-relaxed">
-                            <strong className="text-2xl">Wallpapers exclusivos</strong>
+                            <strong className="text-lg md:text-2xl">Wallpapers exclusivos</strong>
                             <p className="text-sm text-gray-200 mt-2">
                                 Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
                             </p>
                         </div>
-                        <div className="h-full p-6 flex items-center">
+                        <div className="h-full p-6 flex items-center text-blue-500">
                             <CaretRight size={24} />
                         </div>
                     </a>
